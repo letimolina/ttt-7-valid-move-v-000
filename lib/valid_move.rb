cff7fc37b1e568baab825546e63b203d73ea3a65
Leticia Molina <letimolina@gmail.com>
2018-04-21 23:17:24 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   valid = nil
-  if !index.between?(0 , 8) && taken == true
+  if taken == true && !index.between?(0 , 8)
     valid = false
   elsif index.between?(0 , 8) && taken == false
     valid = true
